cf59c8a7c6db8fdf4cb656e890a595428b79b213
jason <sunjc@iata.org>
2018-11-02 14:03:30 +0800

    add logging.properties

--- src/main/resources/check_selenium.sh
+++ src/main/resources/check_selenium.sh
@@ -4,4 +4,4 @@ export DISPLAY=:1
 #xhost +
 JAVA_HOME=/opt/java/jdk1.8.0_77
 
-$JAVA_HOME/bin/java -jar $(dirname $0)/lib/nagios-selenium-1.0.jar $@
+$JAVA_HOME/bin/java -Djava.util.logging.config.file=logging.properties -jar $(dirname $0)/lib/nagios-selenium-1.0.jar $@
