4030e23eece1c8bee44d310f123ae746687be9e1
Sunjc <sun-coco925>
2018-11-06 22:57:25 +0800

    refactor

--- src/main/resources/check_selenium.sh
+++ src/main/resources/check_selenium.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export DISPLAY=:1
-#xhost +
-JAVA_HOME=/opt/java/jdk1.8.0_77
+JAVA_HOME=/opt/java/jdk1.8.0_191
 
 $JAVA_HOME/bin/java -Djava.util.logging.config.file=logging.properties -jar $(dirname $0)/lib/nagios-selenium-1.0.jar $@
